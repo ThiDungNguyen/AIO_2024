920fa97aab0686c59aa8ca1e51ba7eba3df930ea
Nguyen Dung <nguyendung@nguyens-mbp.mynetworksettings.com>
2024-06-22 18:10:48 -0400

    test rm ex3

--- module1_exercise4/chatbot.py
+++ module1_exercise4/chatbot.py
@@ -3,26 +3,26 @@ from hugchat import hugchat
 from hugchat.login import Login
 
 # App title
-st.title (’Simple ChatBot ’)
+st.title ('Simple ChatBot')
 
 # Hugging Face Credentials
 with st.sidebar :
-    st.title (’Login HugChat ’)
-    hf_email = st.text_input (’Enter E- mail :’)
-    hf_pass = st.text_input (’Enter Password :’, type =’password ’)
+    st.title ('Login HugChat')
+    hf_email = st.text_input ('Enter E- mail :')
+    hf_pass = st.text_input ('Enter Password :', type ='password')
     if not ( hf_email and hf_pass ) :
-        st.warning (’Please enter your account !’)
+        st.warning ('Please enter your account !')
     else :
-        st.success (’Proceed to entering your prompt message !’)
+        st.success ('Proceed to entering your prompt message !')
                  
 # Store LLM generated responses
 if " messages " not in st.session_state.keys () :
-    st.session_state.messages = [{" role ": " assistant ", " content ": "How may I help you?"}]
+    st.session_state.messages = [{"role": "assistant", "content": "How may I help you?"}]
 
 # Display chat messages
 for message in st.session_state.messages :
-    with st.chat_message ( message [" role "]) :
-        st.write ( message [" content "])
+    with st.chat_message ( message ["role"]) :
+        st.write ( message ["content"])
 
 # Function for generating LLM response
 def generate_response ( prompt_input , email , passwd ) :
@@ -30,7 +30,7 @@ def generate_response ( prompt_input , email , passwd ) :
     sign = Login(email , passwd)
     cookies = sign.login ()
     # Create ChatBot
-    chatbot = hugchat.ChatBot ( cookies = cookies . get_dict () )
+    chatbot = hugchat.ChatBot ( cookies = cookies.get_dict () )
     return chatbot.chat ( prompt_input )
 
  # User - provided prompt
@@ -40,10 +40,10 @@ if prompt := st.chat_input ( disabled = not ( hf_email and hf_pass ) ) :
         st.write ( prompt )
 
 # Generate a new response if last message is not from assistant
-if st.session_state.messages [ -1][" role "] != " assistant ":
-    with st.chat_message (" assistant ") :
-        with st.spinner (" Thinking ...") :
+if st.session_state.messages [ -1]["role"] != "assistant":
+    with st.chat_message ("assistant") :
+        with st.spinner ("Thinking ...") :
             response = generate_response ( prompt , hf_email , hf_pass )
             st.write(response )
-    message = {" role ": " assistant ", " content ": response }
+    message = {"role": "assistant", "content": response }
     st.session_state.messages.append ( message )
--- module1_exercise4/object_detection.py
+++ module1_exercise4/object_detection.py
@@ -1,10 +1,14 @@
+import numpy as np
+import streamlit as st
+from PIL import Image
 import cv2
 MODEL = "model/MobileNetSSD_deploy.caffemodel"
 PROTOTXT = "model/MobileNetSSD_deploy.prototxt.txt"
 
 
 def process_image(image):
-    blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 0.007843, (300, 300), 127.5)
+    blob = cv2.dnn.blobFromImage(cv2.resize(
+        image, (300, 300)), 0.007843, (300, 300), 127.5)
     net = cv2.dnn.readNetFromCaffe(PROTOTXT, MODEL)
     net.setInput(blob)
     detections = net.forward()
@@ -20,28 +24,25 @@ def annotate_image(image, detections, confidence_threshold=0.5):
         if confidence > confidence_threshold:
             # extract the index of the class label from the ‘detections ‘ ,
             # then compute the (x, y)-coordinates of the bounding box for
-            # the object
-            idx = int(detections[0, 0, i, 1])
+            # the object, idx = int(detections[0, 0, i, 1])
             box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-            (startX, startY, endX, endY) = box.astype(" int")
-            cv2 . rectangle(image, (startX, startY), (endX, endY), 70, 2)
+            (start_x, start_y, end_x, end_y) = box.astype(" int")
+            cv2 . rectangle(image, (start_x, start_y), (end_x, end_y), 70, 2)
     return image
 
-import numpy as np
-from PIL import Image
-import streamlit as st
 
-def main () :
-    st . title ('Object Detection for Images')
-    file = st.file_uploader('Upload Image', type = ['jpg','png','jpeg'])
-    if file is not None :
-        st . image (file , caption = " Uploaded Image ")
-        
-        image = Image.open (file)
-        image = np.array (image)
-        detections = process_image ( image )
-        processed_image = annotate_image ( image , detections )
-        st.image (processed_image,caption = "Processed Image")
-    
-if __name__ == " __main__ ":
-    main ()
+def main():
+    st . title('Object Detection for Images')
+    file = st.file_uploader('Upload Image', type=['jpg', 'png', 'jpeg'])
+    if file is not None:
+        st . image(file, caption=" Uploaded Image ")
+
+        image = Image.open(file)
+        image = np.array(image)
+        detections = process_image(image)
+        processed_image = annotate_image(image, detections)
+        st.image(processed_image, caption="Processed Image")
+
+
+if __name__ == "__main__":
+    main()
