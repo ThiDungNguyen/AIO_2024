2c0c4bbb5d6519af2890418ab2de4fe0f26c946f
Nguyen Dung <nguyendung@nguyens-mbp.mynetworksettings.com>
2024-07-14 14:24:18 -0400

    add vector_matrix_functions.py

---
+++ module_2/module2_exercise2/vector_matrix_functions.py
@@ -0,0 +1,120 @@
+import numpy as np
+
+
+def compute_vector_length(vector):
+    # *********** Your code here ***********
+    print('np.dot(vector,vector', np.dot(vector, vector))
+    len_of_vector_w1 = np.sqrt(np.dot(vector, vector))
+    # len_of_vector_w2 = np.linalg.norm(vector)
+    return len_of_vector_w1
+
+
+def compute_dot_product(vector1, vector2):
+    # *********** Your code here ***********
+    result = np.dot(vector1, vector2)
+    return result
+
+
+def matrix_multi_vector(matrix, vector):
+    # *********** Your code here ***********
+    result = np.dot(matrix, vector)
+    return result
+
+
+def matrix_multi_matrix(matrix1, matrix2):
+    # *********** Your code here ***********
+    result = np.dot(matrix1, matrix2)
+    return result
+
+
+def inverse_matrix(matrix):
+    # *********** Your code here ***********
+    result = np.linalg.inv(matrix)
+    return result
+
+
+def compute_eigenvalues_eigenvectors(matrix):
+    # *********** Your code here ***********
+    eigenvalues, eigenvectors = np.linalg.eig(matrix)
+    return eigenvalues, eigenvectors
+
+
+def compute_cosine(v1, v2):
+    # *********** Your code here ***********
+    dot_product = np.dot(v1, v2)
+    norm_v1 = np.linalg.norm(v1)
+    norm_v2 = np.linalg.norm(v2)
+    cos_sim = dot_product / (norm_v1 * norm_v2)
+    return cos_sim
+
+
+# Q1
+vector = np.array([-2, 4, 9, 21])
+result = compute_vector_length(vector)
+print(round(result, 2))
+
+# Q2:
+v1 = np. array([0, 1, -1, 2])
+v2 = np. array([2, 5, 1, 0])
+result = compute_dot_product(v1, v2)
+print(round(result, 2))
+
+# Q3:
+x = np. array([[1, 2],
+               [3, 4]])
+k = np. array([1, 2])
+print('result \n', x.dot(k))
+
+# Q4:
+x = np. array([[-1, 2],
+               [3, -4]])
+k = np. array([1, 2])
+print('result \n', x@k)
+
+# Q5:
+m = np. array([[-1, 1, 1], [0, -4, 9]])
+v = np. array([0, 2, 1])
+result = matrix_multi_vector(m, v)
+print(result)
+
+# Q6:
+m1 = np. array([[0, 1, 2], [2, -3, 1]])
+m2 = np. array([[1, -3], [6, 1], [0, -1]])
+result = matrix_multi_matrix(m1, m2)
+print(result)
+
+# Q7:
+m1 = np.eye(3)  # creates a 3x3 identity matrix
+m2 = np. array([[1, 1, 1], [2, 2, 2], [3, 3, 3]])
+result = m1@m2
+print(result)
+
+# Q8:
+m1 = np.eye(2)
+m1 = np. reshape(m1, (-1, 4))[0]
+m2 = np. array([[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3], [4, 4, 4, 4]])
+result = m1@m2
+print(result)
+
+# Q9:
+m1 = np. array([[1, 2], [3, 4]])
+m1 = np. reshape(m1, (-1, 4), "F")[0]
+m2 = np. array([[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3], [4, 4, 4, 4]])
+result = m1@m2
+print(result)
+
+# Q10:
+m1 = np. array([[-2, 6], [8, -4]])
+result = inverse_matrix(m1)
+print(result)
+
+# Q11:
+matrix = np. array([[0.9, 0.2], [0.1, 0.8]])
+eigenvalues, eigenvectors = compute_eigenvalues_eigenvectors(matrix)
+print(eigenvectors)
+
+# Q12:
+x = np. array([1, 2, 3, 4])
+y = np. array([1, 0, 3, 0])
+result = compute_cosine(x, y)
+print(round(result, 3))
