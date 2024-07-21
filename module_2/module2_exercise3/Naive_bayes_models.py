f37d62be2ff5c3d50d11d9c3d6c8c2bdf80e60e6
Nguyen Dung <nguyendung@nguyens-mbp.mynetworksettings.com>
2024-07-21 17:16:50 -0400

    module 2 ex3

---
+++ module_2/module2_exercise3/Naive_bayes_models.py
@@ -0,0 +1,58 @@
+
+# ##########################
+# Train Naive Bayes Model
+# ##########################
+from conditional_probability import compute_prior_probablity, create_train_data, get_index_from_value, compute_conditional_probability
+
+
+def train_naive_bayes(train_data):
+    # Step 1: Calculate Prior Probability
+    y_unique = ['no', 'yes']
+    prior_probability = compute_prior_probablity(train_data)
+
+    # Step 2: Calculate Conditional Probability
+    conditional_probability, list_x_name = compute_conditional_probability(
+        train_data)
+
+    return prior_probability, conditional_probability, list_x_name
+
+# ###################
+# Prediction
+# ###################
+
+
+def prediction_play_tennis(x, list_x_name, prior_probability, conditional_probability):
+
+    x1 = get_index_from_value(x[0], list_x_name[0])
+    x2 = get_index_from_value(x[1], list_x_name[1])
+    x3 = get_index_from_value(x[2], list_x_name[2])
+    x4 = get_index_from_value(x[3], list_x_name[3])
+    print("x1 = ", x1)
+    print("x2 = ", x2)
+    print("x3 = ", x3)
+    print("x4 = ", x4)
+
+    p0 = 0
+    p1 = 0
+
+    # your code here ***********************
+
+    if p0 > p1:
+        y_pred = 0
+    else:
+        y_pred = 1
+
+    return y_pred
+
+
+X = ['Sunny', 'Cool', 'High', 'Strong']
+data = create_train_data()
+prior_probability, conditional_probability, list_x_name = train_naive_bayes(
+    data)
+pred = prediction_play_tennis(
+    X, list_x_name, prior_probability, conditional_probability)
+
+if (pred):
+    print("Ad should go!")
+else:
+    print("Ad should not go!")
---
+++ module_2/module2_exercise3/conditional_probability.py
@@ -0,0 +1,98 @@
+import numpy as np
+
+
+def create_train_data():
+
+    data = [['Sunny', 'Hot', 'High', 'Weak', 'no'],
+            ['Sunny', 'Hot', 'High', 'Strong', 'no'],
+            ['Overcast', 'Hot', 'High', 'Weak', 'yes'],
+            ['Rain', 'Mild', 'High', 'Weak', 'yes'],
+            ['Rain', 'Cool', 'Normal', 'Weak', 'yes'],
+            ['Rain', 'Cool', 'Normal', 'Strong', 'no'],
+            ['Overcast', 'Cool', 'Normal', 'Strong', 'yes'],
+            ['Overcast', 'Mild', 'High', 'Weak', 'no'],
+            ['Sunny', 'Cool', 'Normal', 'Weak', 'yes'],
+            ['Rain', 'Mild', 'Normal', 'Weak', 'yes']]
+    return np.array(data)
+
+
+def compute_prior_probablity(train_data):
+    y_unique = ['no', 'yes']
+    prior_probability = np.zeros(len(y_unique))
+    prior_probability[0] = len(
+        train_data[train_data[:, 4] == 'no'])/len(train_data[:, 4])
+    prior_probability[1] = len(
+        train_data[train_data[:, 4] == 'yes'])/len(train_data[:, 4])
+    return prior_probability
+
+
+def compute_conditional_probability(train_data):
+    y_unique = ['no', 'yes']
+    conditional_probability = []
+    list_x_name = []
+    for i in range(0, 4):  # train_data.shape[1] - 1):
+        column_unique = np.unique(train_data[:, i])
+        # print('column_unique', column_unique)
+        list_x_name.append(column_unique)
+        column_conditional_probability = []
+        for item in column_unique:
+            p_sunny_given_yes = len(train_data[((train_data[:, i] == item) & (
+                train_data[:, 4] == 'yes'))])/len(train_data[train_data[:, 4] == 'yes'])
+            p_sunny_given_no = len(train_data[((train_data[:, i] == item) & (
+                train_data[:, 4] == 'yes'))])/len(train_data[train_data[:, 4] == 'no'])
+            column_conditional_probability.append(
+                [p_sunny_given_no, p_sunny_given_yes])
+
+        conditional_probability.append(column_conditional_probability)
+
+    # print('conditional_probability', conditional_probability)
+    # print('conditional_probability[0]', conditional_probability[0])
+    return conditional_probability, list_x_name
+
+
+def get_index_from_value(feature_name, list_features):
+    ''' This function is used to return the index of the feature name'''
+    return np.where(list_features == feature_name)[0][0]
+
+
+train_data = create_train_data()
+print(train_data)
+
+# Q14:
+prior_probablity = compute_prior_probablity(train_data)
+print("P(play tennis = No)", prior_probablity[0])
+print("P(play tennis = Yes)", prior_probablity[1])
+
+# Q15:
+_, list_x_name = compute_conditional_probability(train_data)
+
+print("x1 = ", list_x_name[0])
+print("x2 = ", list_x_name[1])
+print("x3 = ", list_x_name[2])
+print("x4 = ", list_x_name[3])
+
+
+# Q16:
+outlook = list_x_name[0]
+
+i1 = get_index_from_value("Overcast", outlook)
+i2 = get_index_from_value("Rain", outlook)
+i3 = get_index_from_value("Sunny", outlook)
+
+print(i1, i2, i3)
+
+# Q17:
+conditional_probability, list_x_name = compute_conditional_probability(
+    train_data)
+# Compute P(" Outlook "=" Sunny "| Play Tennis "=" Yes ")
+x1 = get_index_from_value("Sunny", list_x_name[0])
+print("P('Outlook'= 'Sunny'| Play Tennis = 'Yes') = ",
+      np.round(conditional_probability[0][x1][1], 2))
+
+
+# Q18:
+conditional_probability, list_x_name = compute_conditional_probability(
+    train_data)
+# Compute P(" Outlook "=" Sunny "| Play Tennis "=" No ")
+x1 = get_index_from_value("Sunny", list_x_name[0])
+print("P('Outlook'= 'Sunny'| Play Tennis = 'No') = ", np.round(conditional_probability[0][x1][0], 2))
